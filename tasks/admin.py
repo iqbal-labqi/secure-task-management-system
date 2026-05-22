3fabcef9322ba42045fa2829fd6bc158b208c647
LuQkun <157957672+LuQkun@users.noreply.github.com>
2026-05-22 12:34:08 +0800

    Delete tasks directory

--- tasks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display  = ('title', 'owner', 'status', 'priority', 'due_date')
-    list_filter   = ('status', 'priority')
-    search_fields = ('title', 'owner__username')
